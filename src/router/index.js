7b58e78ac938d6f3c89a3f1dd8613293a997b460
sky <jxsdifens@163.com>
2018-03-12 19:29:00 +0800

    添加页面构架

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+
+import Main from '@/views/Main'
+import Home from '@/views/Home'
+import Explorer from '@/views/Explorer'
+import Cart from '@/views/Cart'
+import My from '@/views/My'
+import BookDetail from '@/views/BookDetail'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: __dirname,
+  linkActiveClass: "active",
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      // name: 'Main',
+      component: Main,
+      children: [
+        {path: '/',name: 'Home',component: Home},
+        {path: 'explorer',name: 'Explorer',component: Explorer},
+        {path: 'cart',name: 'Cart',component: Cart},
+        {path: 'my',name: 'My',component: My}
+      ]
+    },
+    {path: '/book/:id', name: 'BookDetail', component: BookDetail}
   ]
 })
---
+++ src/views/Main.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    主窗口Main
+    <router-view/>
+    
+    <mt-tabbar>
+      <mt-tab-item id="home">
+        <router-link :to="{name: 'Home'}">
+          首页
+        </router-link>
+      </mt-tab-item>
+      <mt-tab-item id="explorer">
+        <router-link :to="{name: 'Explorer'}">
+          分类
+        </router-link>
+      </mt-tab-item>
+      <mt-tab-item id="cart">
+        <router-link :to="{name: 'Cart'}">
+          购物车
+        </router-link>
+      </mt-tab-item>
+      <mt-tab-item id="my">
+        <router-link :to="{name: 'My'}">
+          我的
+        </router-link>
+      </mt-tab-item>
+    </mt-tabbar>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
